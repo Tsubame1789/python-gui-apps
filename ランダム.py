87630be198bf796069e94990eeeadfd05de00ea1
Tsubame1789 <javasparrow0914@gmail.com>
2024-06-19 12:16:46 +0900

    クッキー

---
+++ ランダム.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+import random
+
+# ↓↓↓ お約束のコード ↓↓↓
+window = tk.Tk()
+window.title("GUI App")
+window.geometry("600x400")
+bg_color = "#333333"  # ダークグレー
+fg_color = "#FFFFFF"  # 白
+window.configure(bg=bg_color)
+# ↑↑↑ お約束のコード ↑↑↑
+
+team = ["太宰", "中也", "フョードル", "ニコライ", "シグマ"]
+
+# ボタンの作成
+button1 = tk.Button(window, text="ランダム", command=lambda: button_action())
+button1.pack(pady=10)
+
+# 出力ラベルの作成
+label1 = tk.Label(window, text="", bg=bg_color, fg=fg_color)
+label1.pack(pady=10)
+
+
+def button_action():
+    member = random.choice(team)
+    label1.config(text=member)
+
+
+# ↓↓↓ お約束のコード ↓↓↓
+window.mainloop()
+# ↑↑↑ お約束のコード ↑↑↑
